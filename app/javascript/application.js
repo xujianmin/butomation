4823e09bf35b34e230c27e02c5903ad8b7185310
Jimmy Xu <jianmin.xu@usingnow.com>
2025-05-20 00:06:30 +0800

    move top navbar toggle into a stimulus controller to avoid the conflicts.

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,12 +2,3 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
-
-// offcanvas-navbar.js
-(() => {
-  'use strict'
-
-  document.querySelector('#navbarSideCollapse').addEventListener('click', () => {
-    document.querySelector('.offcanvas-collapse').classList.toggle('open')
-  })
-})()
