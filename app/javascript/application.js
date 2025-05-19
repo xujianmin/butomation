5a106b44d6f697a325c3667daf06a7ce40bac9e2
Jimmy Xu <jianmin.xu@usingnow.com>
2025-05-19 21:25:22 +0800

    add bootstrap layout with offcanvas navbar.

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,12 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
+
+// offcanvas-navbar.js
+(() => {
+  'use strict'
+
+  document.querySelector('#navbarSideCollapse').addEventListener('click', () => {
+    document.querySelector('.offcanvas-collapse').classList.toggle('open')
+  })
+})()
