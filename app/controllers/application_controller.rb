6b79594764a330614d5722aae51c9bf981a1821c
Jimmy Xu <jianmin.xu@usingnow.com>
2025-06-11 02:16:24 +0800

    ref #10 add a logout button in top nav and set an expired_at 15 mins auto-logout.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,22 @@ class ApplicationController < ActionController::Base
   include Authentication
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  # before_action :authenticate
+  before_action :check_session_expiry
+
+  private
+
+  def check_session_expiry
+    if session[:expires_at] && session[:expires_at] < Time.current
+      terminate_session
+      redirect_to new_session_path, alert: "会话已过期，请重新登录"
+    end
+  end
+
+  def terminate_session
+    Current.session&.destroy
+    cookies.delete(:session_id)
+    session.clear
+  end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < ApplicationController
   allow_unauthenticated_access only: %i[ new create ]
-  rate_limit to: 10, within: 3.minutes, only: :create, with: -> { redirect_to new_session_url, alert: "Try again later." }
+  rate_limit to: 10, within: 3.minutes, only: :create, with: -> { redirect_to new_session_url, alert: "重试次数过多，请稍后再试！" }
 
   def new
   end
@@ -8,7 +8,8 @@ class SessionsController < ApplicationController
   def create
     if user = User.authenticate_by(params.permit(:email_address, :password))
       start_new_session_for user
-      redirect_to after_authentication_url
+      session[:expires_at] = 15.minutes.from_now
+      redirect_to after_authentication_url, notice: "登入成功!"
     else
       redirect_to new_session_path, alert: "邮箱地址或密码错误！"
     end
@@ -16,6 +17,6 @@ class SessionsController < ApplicationController
 
   def destroy
     terminate_session
-    redirect_to new_session_path
+    redirect_to new_session_path, notice: "已退出登入"
   end
 end
